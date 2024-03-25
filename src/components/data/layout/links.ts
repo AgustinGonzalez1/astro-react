ba0d2629671afc3c0725ea1a986cf1d0ee15d4bb
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-03-25 20:24:21 -0300

    footer mobile, navbar and more pages

---
+++ src/components/data/layout/links.ts
@@ -0,0 +1,23 @@
+export interface ILink {
+	title: string;
+	url: string;
+}
+
+export const linksData: ILink[] = [
+	{
+		title: "Empresas",
+		url: "/",
+	},
+	{
+		title: "Developers",
+		url: "/developers",
+	},
+	{
+		title: "Empleos",
+		url: "/empleos",
+	},
+	{
+		title: "Blog",
+		url: "/blog",
+	},
+];
